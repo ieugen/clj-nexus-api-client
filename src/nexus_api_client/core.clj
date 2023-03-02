d247c78d9402ebb964fcde5d52aac0bec7519433
Andrei <sandre1>
2023-03-02 16:19:38 +0200

    get the ops from the nexux api file

--- src/nexus_api_client/core.clj
+++ src/nexus_api_client/core.clj
@@ -1,1 +1,101 @@
-(ns nexus-api-client.core)
+(ns nexus-api-client.core
+  (:require
+   [nexus-api-client.jvm-runtime :as rt]
+   [nexus-api-client.interface :as int]))
+
+(defn categories
+  "Returns the available categories for an engine at a specified verison.
+  Categories are the kind of operations the engine can do.
+  eg. :docker and v1.41
+      :podman and v3.2.3"
+  [engine version]
+  (->> (int/load-api) ;;TODO: add params
+       (keys)
+       (int/remove-internal-meta)))
+
+(defn client
+  "Creates a client scoped to an engine, category, connection settings and API version.
+  Connection settings:
+  uri: The full URI with the protocol for the connection to the engine.
+  read-timeout: Read timeout in ms.
+  write-timeout: Write timeout in ms.
+  call-timeout: Total round trip timeout in ms.
+  mtls: A map having the paths to the CA, key and cert to perform Mutual TLS with the engine."
+  [{:keys [engine category conn version]}]
+  (let [api (int/load-api) ;;TODO: add params
+        {:keys [uri
+                connect-timeout
+                read-timeout
+                write-timeout
+                call-timeout
+                mtls]}
+        conn]
+    {:category category
+     :api (-> api
+              category
+              (merge (select-keys api [:contajners/doc-url])))
+     :conn (rt/client uri
+                      {:connect-timeout-ms connect-timeout
+                       :read-timeout-ms read-timeout
+                       :write-timeout-ms write-timeout
+                       :call-timeout-ms call-timeout
+                       :mtls mtls})
+     :version version}))
+
+(defn ops
+  "Returns the supported operations for a client."
+  [{:keys [api]}]
+  (->> api
+       (keys)
+       (int/remove-internal-meta)))
+
+(defn doc
+  "Returns the summary and doc URL of the operation in the client."
+  [{:keys [version api]} op]
+  (some-> api
+          op
+          (select-keys [:summary])
+          (assoc :doc-url
+                 (format (:contajners/doc-url api)
+                         version
+                         (name op)))))
+
+(defn invoke
+  "Performs the operation with the specified client and a map of options.
+  Options map:
+  op: The operation to invoke on the engine. Required.
+  params: The params needed for the operation. Default: {}.
+  data: The payload needed to be sent to the op. Maps will be JSON serialized. Corresponds to the Request Body in docs. Default: {}.
+  as: The return type of the response. :data, :stream, :socket. Default: :data.
+  throw-exceptions: Throws exceptions when status is >= 400 for API calls. Default: false.
+  throw-entire-message: Includes the full exception as a string. Default: false."
+  [{:keys [version conn api category]} {:keys [op params data as throw-exceptions throw-entire-message]}]
+  (if-let [operation (op api)]
+    (let [request-params (reduce (partial int/gather-params params)
+                                 {}
+                                 (:params operation))
+          request {:client conn
+                   :method (:method operation)
+                   :path (-> operation
+                             :path
+                             (int/interpolate-path (:path request-params))
+                             (as-> path (str "/" version path)))
+                   :headers (:header request-params)
+                   :query-params (:query request-params)
+                   :body data
+                   :as as
+                   :throw-exceptions throw-exceptions
+                   :throw-entire-message throw-entire-message}
+          response (-> request
+                       (int/maybe-serialize-body)
+                       (rt/request))]
+      (case as
+        (:socket :stream) response
+        (int/try-json-parse response)))
+    (int/bail-out (format "Invalid operation %s for category %s"
+                           (name op)
+                           (name category)))))
+
+(comment 
+  (+ 2 2)
+  0)
--- src/nexus_api_client/interface.clj
+++ src/nexus_api_client/interface.clj
@@ -1,1 +1,107 @@
-(ns nexus-api-client.interface)
+(ns nexus-api-client.interface
+  (:require
+   [clojure.data.json :as json]
+   [nexus-api-client.jvm-runtime :as nrt]
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
+   [clojure.string :as s]
+   [nexus-api-client.interface :as int])
+  (:import
+   [java.io PushbackReader]
+   [java.util.regex Pattern]))
+
+(defn bail-out
+  [^String message]
+  (throw (IllegalArgumentException. message)))
+
+(defn remove-internal-meta
+  "Removes keywords namespaced with :contajners. They are for internal use."
+  [data-seq]
+  (remove #(= "contajners" (namespace %)) data-seq))
+
+(defn load-api
+  "Loads the API EDN file from resources."
+  [version]
+  (let [config "resources/sonatype-nexus/api.edn"]
+    (-> config
+        (io/reader)
+        (PushbackReader.)
+        (edn/read)
+        (version))
+    #_(bail-out "Cannot load api, the engine, version combo may not be supported.")))
+
+(defn gather-params
+  "Reducer fn categorizing the params as :header, :query or :path.
+  supplied-params: map of params the user has passed when invoking.
+  request-params: accumulator for all the params to be actually sent.
+  each param in the spec is passed as the last arg and categorized by `in` if it is supplied."
+  [supplied-params request-params {:keys [name in]}]
+  (let [param (keyword name)]
+    (if-not (contains? supplied-params param)
+      request-params
+      (update-in request-params [(keyword in)] assoc param (param supplied-params)))))
+
+(defn maybe-serialize-body
+  "If the body is a map, convert it to JSON and attach the correct headers."
+  [{:keys [body] :as request}]
+  (if (map? body)
+    (-> request
+        (assoc-in [:headers "content-type"] "application/json")
+        (update :body json/write-str))
+    request))
+
+(defn interpolate-path
+  "Replaces all occurrences of {k1}, {k2} ... with the value map provided.
+  Example:
+  given a/path/{id}/on/{not-this}/root/{id} and {:id hello}
+  results in: a/path/hello/{not-this}/root/hello."
+  [path value-map]
+  (let [[param value] (first value-map)]
+    (if (nil? param)
+      path
+      (recur (s/replace path
+                        (re-pattern (format "\\{([%s].*?)\\}"
+                                            (-> param
+                                                name
+                                                Pattern/quote)))
+                        (str value))
+             (dissoc value-map param)))))
+
+(defn try-json-parse
+  "Attempts to parse `value` as a JSON string. no-op if its not a valid JSON string."
+  [value]
+  (try
+    (json/read-str value :key-fn keyword)
+    (catch Exception _ value)))
+
+(comment 
+  (+ 2 2)
+
+  (remove-internal-meta [:contajners/foo :foo])
+  (try-json-parse "[1, 2, 3]")
+  (try-json-parse "yesnt")
+
+ (def client (nrt/client "tcp://localhost:8080" {}))
+
+(nrt/request {:client client :url "/v1.40/_ping" :method :get})
+
+(nrt/request {:client client :url "/containers/json" :method :get})
+
+(reduce (partial gather-params {:a 42 :b 64 :c 44})
+        {}
+        [{:name "a" :in :path}
+         {:name "b" :in :query}
+         {:name "c" :in :query}])
+
+(maybe-serialize-body {:body {:a 42}})
+
+(maybe-serialize-body {:body 42})
+
+(interpolate-path "/a/{w}/b/{x}/{y}" {:x 41 :y 42 :z 43})
+(load-api :v1)
+  (let [api (load-api :v1)]
+    (->> api 
+         (keys)
+         (int/remove-internal-meta)))
+
+  0)
