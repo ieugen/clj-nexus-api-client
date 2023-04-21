32796658207f7f63fd014896f039e5c419128fdd
Eugen Stan <eugen.stan@netdava.com>
2023-04-21 17:31:08 +0300

    Ceva exemplu

--- examples/simple/src/demo/core.clj
+++ examples/simple/src/demo/core.clj
@@ -2,11 +2,15 @@
   (:require [nexus-api-client.core :as c]
             [nexus-api-client.interface :as interface]))
 
-(comment 
-  
+(comment
+
   (c/doc (interface/load-api) :getRepositories)
-  
+
   (System/getenv "NEXUS_ADMIN_PASS")
-  
-  
+
+
+  (let [uri (java.net.URI/create "file:///var/run/docker.sock")]
+    (println uri
+             "host: " (.getHost uri) ))
+
   )
