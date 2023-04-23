8a80f4ad40b4b798bfa8a7db1b76407941ac3abf
Andrei <sandre1>
2023-04-23 17:39:29 +0300

    Try catch error ..try
    Signed-off-by: Andrei <sandre1>

--- src/nexus_api_client/interface.clj
+++ src/nexus_api_client/interface.clj
@@ -30,38 +30,71 @@
   params: The params needed for the operation. Default: {}."
 
   [conn-opts invoke-opts]
+
   (let [url (:endpoint conn-opts)
         user (-> conn-opts :creds :user)
         pass (-> conn-opts :creds :pass)
-        api (core/load-api)
+
         op-name (:operation invoke-opts)
-        ops-opts (doc api op-name)
-        supplied-params (:params invoke-opts)
-        ops-params (:params ops-opts)
-        request-params (reduce (partial core/gather-params supplied-params)
-                               {}
-                               ops-params)
-        query-params (:query request-params)
-        interpolate-path-opts (:path request-params)
-        method (name (:method ops-opts))
-        path (:path ops-opts)
-        new-path (core/interpolate-path path interpolate-path-opts)
-        invoke-url (if (empty? query-params)
-                     (str url new-path)
-                     (str url new-path "?" (core/create-query query-params)))
-        response-body (:body (core/api-request method invoke-url))]
-    (core/json->edn response-body)))
+
+        supplied-params (:params invoke-opts)]
+    (try
+      (let [api (core/load-api)
+            ops-opts (doc api op-name)
+            ops-params (:params ops-opts)
+            request-params (reduce (partial core/gather-params supplied-params)
+                                   {}
+                                   ops-params)
+            query-params (:query request-params)
+            interpolate-path-opts (:path request-params)
+            method (name (:method ops-opts))
+            path (:path ops-opts)
+            new-path (core/interpolate-path path interpolate-path-opts)
+            invoke-url (if (empty? query-params)
+                         (str url new-path)
+                         (str url new-path "?" (core/create-query query-params)))
+            response-body (:body (core/api-request method invoke-url))]
+        (core/json->edn response-body))
+      (catch Exception e (prn (let [api (core/load-api)
+                                    ops-opts (doc api op-name)
+                                    ops-params (:params ops-opts)
+                                    request-params (reduce (partial core/gather-params supplied-params)
+                                                           {}
+                                                           ops-params)
+                                    query-params (:query request-params)
+                                    interpolate-path-opts (:path request-params)
+                                    method (name (:method ops-opts))
+                                    path (:path ops-opts)
+                                    new-path (core/interpolate-path path interpolate-path-opts)
+                                    invoke-url (if (empty? query-params)
+                                                 (str url new-path)
+                                                 (str url new-path "?" (core/create-query query-params)))]
+                                (str "caught exception: \n" (.getMessage e)
+                                     "\n Request: " invoke-url
+                                     "\n Response: " (core/api-request method invoke-url))))))))
+
+
+
+
 
 (comment
   (invoke {:endpoint "http://localhost:8081/service/rest"
            :creds {:user "admin" :pass "admin"}}
-          {:operation :getRepository
+          {:operatio :getRepository
            :params {:repositoryName "docker"}})
-  
-  (invoke {:endpoint "http://localhost:8081/service/rest"
+
+  (invoke {:endpoint "http://localhost:8081/service/res"
            :creds {:user "admin" :pass "admin"}}
           {:operation :getAssetById
            :params {:id "bWF2ZW4tY2VudHJhbDozZjVjYWUwMTc2MDIzM2I2MjRiOTEwMmMwMmNiYmU4YQ"}})
+
+  (try
+    (map "fst")
+    (throw
+     (ex-info "The ice cream has melted!"
+              {:causes             #{:fridge-door-open :dangerously-high-temperature}
+               :current-temperature {:value 25 :unit :celsius}}))
+    (catch Exception e (ex-data e)))
 
 
   (invoke {:endpoint "http://localhost:8081/service/rest"
@@ -74,5 +107,4 @@
 
   (ops (core/load-api))
 
-  0
-  )
+  0)
